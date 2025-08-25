8272430765ede7536f18b85afb315317b97e9287
lemng32 <lamnguyenmai92@gmail.com>
2025-08-25 15:44:55 -0500

    Added plugin to render markdown

--- nvim/.config/nvim/lua/config/set.lua
+++ nvim/.config/nvim/lua/config/set.lua
@@ -31,3 +31,5 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.g.markdown_recommended_style = 0
---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+  ft = { 'markdown', 'quatro' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    completions = { blink = { enabled = true } },
+  },
+}
